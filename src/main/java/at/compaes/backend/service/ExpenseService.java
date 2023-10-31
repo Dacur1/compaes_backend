d77c117ec406d8091f1c097eb2954b73f4663998
David <david281100@hotmail.com>
2023-10-31 19:54:29 +0100

    added expense service class

---
+++ src/main/java/at/compaes/backend/service/ExpenseService.java
@@ -0,0 +1,36 @@
+package at.compaes.backend.service;
+
+import at.compaes.backend.model.Expense;
+import at.compaes.backend.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ExpenseService {
+
+    private final ExpenseRepository expenseRepository;
+
+    @Autowired
+    public ExpenseService(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
+    public List<Expense> findAllExpenses() {
+        return expenseRepository.findAllExpenses();
+    }
+
+    public List<Expense> findAllByOrderByDateDesc() {
+        return expenseRepository.findAllByOrderByDateDesc();
+    }
+
+    public List<Expense> findAllByCategory(String category) {
+        return expenseRepository.findAllByCategory(category);
+    }
+
+    public List<Expense> findAllByPaymentMethod(String paymentMethod) {
+        return expenseRepository.findAllByPaymentMethod(paymentMethod);
+    }
+
+}
