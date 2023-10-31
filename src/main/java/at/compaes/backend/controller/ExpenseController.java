a5104e337ff950c32cde840470cea1add94d61a0
David <david281100@hotmail.com>
2023-10-31 20:21:25 +0100

    added expense controller class and corresponding get endpoints

---
+++ src/main/java/at/compaes/backend/controller/ExpenseController.java
@@ -0,0 +1,64 @@
+package at.compaes.backend.controller;
+
+import at.compaes.backend.model.Expense;
+import at.compaes.backend.service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/expenses")
+public class ExpenseController {
+
+    private final ExpenseService expenseService;
+    @Autowired
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Expense>> getAllExpenses() {
+        List<Expense> expenses = expenseService.findAllExpenses();
+
+        if (expenses.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(expenses, HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Expense>> getAllExpenses(@RequestParam(required = false) String sort) {
+        List<Expense> expenses;
+
+        if ("asc".equals(sort)) {
+            expenses = expenseService.findAllByOrderByDateAsc();
+        } else if ("desc".equals(sort)) {
+            expenses = expenseService.findAllByOrderByDateDesc();
+        } else {
+            expenses = expenseService.findAllExpenses();
+        }
+
+        if (expenses.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(expenses, HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Expense>> getExpensesByCategory(@RequestParam(required = false) String category) {
+        List<Expense> expenses;
+
+        if (category != null) {
+            expenses = expenseService.findAllByCategory(category);
+        } else {
+            expenses = expenseService.findAllExpenses();
+        }
+        if (expenses.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(expenses, HttpStatus.OK);
+    }
+}
