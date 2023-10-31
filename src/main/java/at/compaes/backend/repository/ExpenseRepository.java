03207dc64437f59b7be039dfc01b4f1244617540
David <david281100@hotmail.com>
2023-10-31 19:50:02 +0100

    added repo annotation

--- src/main/java/at/compaes/backend/repository/ExpenseRepository.java
+++ src/main/java/at/compaes/backend/repository/ExpenseRepository.java
@@ -2,9 +2,11 @@ package at.compaes.backend.repository;
 
 import at.compaes.backend.model.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
     List<Expense> findAllExpense();
